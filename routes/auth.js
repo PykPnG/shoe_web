cfa6d96284dbf3a1bab3e38f8e95a9ee353f8aeb
PngPyK <prm.frank@gmail.com>
2023-03-08 00:35:43 +0700

    feat: shoe1-complate

---
+++ routes/auth.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+let userID = '';
+
+router.get('/', function (req, res, next) {
+   userID = req.session.user;
+   res.render('index', { title: 'Home', userID: userID });
+});
+
+router.get('/login', function (req, res, next) {
+   res.render('login', { title: 'Login' });
+});
+
+router.get('/register', function (req, res, next) {
+   res.render('register', { title: 'Register' });
+});
+
+
+
+router.get('/logout', function (req, res, next) {
+   req.session.user = '';
+   userID = '';
+   res.redirect('/');
+});
+
+router.post('/login', function (req,res){
+   const {email, password} = req.body;
+   const user = {username: "Jane Doe"}
+   req.session.user = user;
+   res.redirect('/');
+})
+
+router.post('/register', function (req,res){
+   const {name, email, birthDate, address, password} = req.body;
+   msg = 'ลงทะเบียนเสร็จสิ้น กรุณาล็อคอินอีกครั้ง';
+   path = 'login';
+   res.redirect('/success/' + msg + '/' + path);
+})
+
+router.get('/success/:msg/:path', async (req, res, next) => {
+   console.log("Welcome");
+   const { msg, path } = req.params
+   res.render('success', { success: msg, path: path });
+})
+
+module.exports = router;
